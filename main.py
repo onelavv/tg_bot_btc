1110c6e586d7a8bf842ec9e92b99914a365b37e6
onelavv <efim.makarenkov@gmail.com>
2022-06-18 00:54:25 +0400

    tg_bot_btc

---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+from datetime import datetime
+import telebot
+from auth_data import token
+
+
+def get_data():
+    req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+    response = req.json()
+    sell_price = response["btc_usd"]["sell"]
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}")
+
+
+def tg_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["start"])
+    def start_message(message):
+        bot.send_message(message.chat.id, 'Hello friend, please write "price" for know sell price for BTC')
+
+    @bot.message_handler(content_types=["text"])
+    def send_message(message):
+        if message.text.lower() == "price":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+                response = req.json()
+                sell_price = response["btc_usd"]["sell"]
+                bot.send_message(
+                    message.chat.id,
+                    f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}"
+                )
+            except Exception as ex:
+                print(ex)
+                bot.send_message(
+                    message.chat.id,
+                    "Something was wrong"
+                )
+        else:
+            bot.send_message(
+                message.chat.id,
+                "wrong command"
+            )
+
+    bot.polling()
+
+
+if __name__ == '__main__':
+    #get_data()
+    tg_bot(token)
